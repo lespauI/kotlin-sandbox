123fc8d858b959a54c507291e9953d34799e791d
Konstantin Pletenev <konstantin.pletenev@accenture.com>
2020-09-02 15:13:23 +0300

    add spring boot

--- ufl-bot/src/main/kotlin/UflBotApplication.kt
+++ ufl-bot/src/main/kotlin/UflBotApplication.kt
@@ -1,14 +1,16 @@
 import com.elbekD.bot.Bot
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 
+@SpringBootApplication
 open class UflBotApplication
-    fun main(args: Array<String>) {
-        val token = ""
-        val bot = Bot.createPolling("" , token)
 
-        bot.onCommand("/start") { msg, _ ->
-                bot.sendMessage(msg.chat.id, "Hello World!")
-        }
-        bot.start()
+fun main(args: Array<String>) {
+    val token = System.getProperty("token")
+    val bot = Bot.createPolling("", token)
+
+    bot.onCommand("/start") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Hello World!")
     }
-
+    bot.start()
+}
