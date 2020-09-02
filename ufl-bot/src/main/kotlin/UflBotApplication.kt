5857b85b454faacdf114b619ab27005f083a4662
Konstantin Pletenev <konstantin.pletenev@accenture.com>
2020-09-02 15:51:08 +0300

    add plan for bot

--- ufl-bot/src/main/kotlin/UflBotApplication.kt
+++ ufl-bot/src/main/kotlin/UflBotApplication.kt
@@ -12,5 +12,43 @@ fun main(args: Array<String>) {
     bot.onCommand("/start") { msg, _ ->
         bot.sendMessage(msg.chat.id, "Hello World!")
     }
+
+    bot.onCommand("/set") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Executing set")
+        //set to database
+    }
+
+    bot.onCommand("/get") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Executing get")
+        //get from dbase
+    }
+
+    bot.onCommand("/waiver") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Executing waiver")
+        /* methods for waiver logic
+
+            Объявляется вейвер со списком игроков
+                1) возможность добавить список
+                2) возможность посмотреть список (ссылки на дядю)
+                3) возможность зааплаится на игрока
+                    3.1 в базе создается связь владелец - игрок
+                4) Список всех команд и их очередность на вейвере
+                    4.1 возможность просмотреть
+                    4 2 возможность редактировать
+                5) лимит не более 2 вейверов в сезон
+                6) победитель вейвера переходит в конец очереди
+
+         */
+    }
+
+    bot.onCommand("/elo") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Executing elo")
+        /* methods for waiver logic
+            1. гет эло
+            2. сет эло
+            3. апдейт эло
+        */
+    }
+
     bot.start()
 }
